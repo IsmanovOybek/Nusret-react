925dc01fb8f9658bb7d9bbf07808b85a3ccaa35e
IsmanovOybek <Ismanovoybek75@gmail.com>
2025-09-05 14:30:12 +0900

    fix; modify login

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -6,7 +6,7 @@ import Fade from "@material-ui/core/Fade";
 import { Fab, Stack, TextField } from "@mui/material";
 import styled from "styled-components";
 import LoginIcon from "@mui/icons-material/Login";
-import { MemberInput } from "../../../libs/types/member";
+import { LoginInput, MemberInput } from "../../../libs/types/member";
 import { Messages } from "../../../libs/config";
 import { T } from "../../../libs/types/common";
 import { sweetErrorHandling } from "../../../libs/sweetAlert";
@@ -63,9 +63,10 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
   const handlePasswordKeyDown = (e: T) => {
     if (e.key === "Enter" && signupOpen) {
       handleSignupRequest().then();
+    } else if (e.key === "Enter" && loginOpen) {
+      handleLoginRequest().then();
     }
   };
-
   const handleSignupRequest = async () => {
     try {
       console.log("input:", memberNick, memberPassword, memberPhone);
@@ -85,6 +86,25 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
     } catch (err) {
       console.log(err);
       handleSignupClose();
+      sweetErrorHandling(err).then();
+    }
+  };
+  const handleLoginRequest = async () => {
+    try {
+      const isFullFill = memberNick !== "" && memberPassword !== "";
+      if (!isFullFill) throw new Error(Messages.error3);
+
+      const loginInput: LoginInput = {
+        memberNick: memberNick,
+        memberPassword: memberPassword,
+      };
+      const member = new MemberService();
+      const result = await member.login(loginInput);
+
+      handleLoginClose();
+    } catch (err) {
+      console.log(err);
+      handleLoginClose();
       sweetErrorHandling(err).then();
     }
   };
@@ -179,17 +199,21 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
                 label="username"
                 variant="outlined"
                 sx={{ my: "10px" }}
+                onChange={handleUserName}
               />
               <TextField
                 id={"outlined-basic"}
                 label={"password"}
                 variant={"outlined"}
                 type={"password"}
+                onChange={handlePassword}
+                onKeyDown={handlePasswordKeyDown}
               />
               <Fab
                 sx={{ marginTop: "27px", width: "120px" }}
                 variant={"extended"}
                 color={"primary"}
+                onClick={handleLoginRequest}
               >
                 <LoginIcon sx={{ mr: 1 }} />
                 Login
